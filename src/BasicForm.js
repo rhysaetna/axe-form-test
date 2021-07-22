567254f1a1450628e0d39e4681399a50ed47ae87
rhysaetna <60111678+rhysaetna@users.noreply.github.com>
2021-07-22 11:00:07 -0600

    fixed BasicForm to pass axe test

--- src/BasicForm.js
+++ src/BasicForm.js
@@ -3,17 +3,24 @@ import "./App.css";
 const BasicForm = () => {
   return (
     <div className="App">
-      <div>
-        <h5>First name</h5>
+      <form>
+        <label htmlFor="firstName">First name</label>
         <input
+          id="firstName"
           name="firstName"
           type="text"
           placeholder="Enter your first name"
         />
         <h5>Gender</h5>
-        Male <input type="radio" name="gender" value="Male" />
-        Female <input type="radio" name="gender" value="Female" />
-      </div>
+        <label>
+          Male 
+          <input type="radio" name="gender" value="Male" />
+        </label>
+        <label>
+          Female 
+          <input type="radio" name="gender" value="Female" />
+        </label>
+      </form>
     </div>
   );
 };
